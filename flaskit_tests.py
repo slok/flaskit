016766531edcf1b7e6c1423049f0b7e8895a9b01
Xabier Larrakoetxea <slok69@gmail.com>
2012-05-01 22:58:16 +0200

    Start adding tests

---
+++ flaskit_tests.py
@@ -0,0 +1,34 @@
+import os
+import flaskit
+import unittest
+import tempfile
+
+class FlaskitTestCase(unittest.TestCase):
+
+    def setUp(self):
+        flaskit.app.config['TESTING'] = True
+        self.app = flaskit.app.test_client()
+
+    def tearDown(self):
+        pass
+        
+    def test_list_repos(self):
+        rv = self.app.get('/')
+        assert '<a href="/flaskit/tree/master/">' in rv.data
+    
+    def test_list_repo_files(self):
+        rv = self.app.get('/flaskit/tree/master/')
+        
+        #Check that are some files and the readme
+        assert '<a href="/flaskit/tree/master/README.md/">' in rv.data
+        assert '<a href="/flaskit/tree/master/requeriments.txt/">' in rv.data
+        assert '<a href="/flaskit/tree/master/filters.py/">' in rv.data
+        assert '<a href="/flaskit/tree/master/flaskit.py/">' in rv.data
+        assert '<a href="/flaskit/tree/master/.gitignore/">' in rv.data
+        assert '<a href="/flaskit/tree/master/templates/">' in rv.data
+        assert '<a href="/flaskit/tree/master/static/">' in rv.data
+        assert '<a href="/flaskit/tree/master/settings.py/">' in rv.data
+        assert '<h2>  <i class="icon-book"></i>   README.md</h2>' in rv.data
+        
+if __name__ == '__main__':
+    unittest.main()
--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ DEBUG = True
 SECRET_KEY = '\x0f\xfc\xd9\x02\x84\x9a\xa3oJ`-\xffK\x1agX\x94\xc5l{\x83\xbf$\xe4'
 
 REPOS = {
-            'mdissphoto':'/home/slok/workspace/mdissphoto/.git',
-            'django-chameleon':'/home/slok/django-chameleon.git',
+            #'mdissphoto':'/home/slok/workspace/mdissphoto/.git',
+            #'django-chameleon':'/home/slok/django-chameleon.git',
+            'flaskit':'.git',
 }
